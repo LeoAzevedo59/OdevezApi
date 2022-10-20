f7811d4861ca48c90c62d9808e13b1d487ec3a7f
Leo Azevedo <leofrancoazevedo@gmail.com>
2022-10-19 21:07:26 -0300

    publish deploy

--- OdevezApi/Startup.cs
+++ OdevezApi/Startup.cs
@@ -93,12 +93,9 @@ namespace OdevezApi
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "OdevezApi v1"));
-            }
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "OdevezApi v1"));
 
             app.UseRouting();
 
